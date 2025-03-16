a25620c3331bb58914da18f2a68edb29397278c2
MadeSatya5 <madesatya505@gmail.com>
2025-03-16 21:46:21 +0700

    feat: add form input post

--- src/components/ui/FormPost.jsx
+++ src/components/ui/FormPost.jsx
@@ -1,13 +1,32 @@
-import ProfilePicture from "./ProfilePicture"
+import { useState } from "react";
+import ProfilePicture from "./ProfilePicture";
 
 function FormPost() {
-    return (
-        <form className="form-post">
-            <ProfilePicture />
-            <input type="text" placeholder="What's Going On?"/>
-            <button>Post</button>
-        </form>
-    )
+  const [inputPost, setInputPost] = useState("");
+
+  function handleSubmit(e) {
+      e.preventDefault();
+      
+      if(!inputPost) return;
+      console.log(inputPost);
+
+      setInputPost("");
+  }
+
+  return (
+    <form className="form-post" onSubmit={handleSubmit}>
+      <div className="input-container">
+        <ProfilePicture />
+        <input
+          type="text"
+          placeholder="What's Going On?"
+          value={inputPost}
+          onChange={(e) => setInputPost(e.target.value)}
+        />
+      </div>
+      <button>Post</button>
+    </form>
+  );
 }
 
-export default FormPost
+export default FormPost;
