520123434f1ed8ba260120eefb2268cf2ba70f3c
MadeSatya5 <madesatya505@gmail.com>
2025-03-20 21:39:39 +0700

    feat: add localstorage posts

--- src/components/layout/Main.jsx
+++ src/components/layout/Main.jsx
@@ -2,9 +2,13 @@ import { useState } from "react";
 import FormPost from "../ui/FormPost";
 import MainNav from "../ui/MainNav";
 import PostList from "../ui/PostList";
+import { useEffect } from "react";
 
 function Main() {
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState(() => {
+    const storedPosts = localStorage.getItem("posts");
+    return storedPosts ? JSON.parse(storedPosts) : [];
+  });
   console.log(posts);
 
   function handleAddPost(post) {
@@ -13,6 +17,12 @@ function Main() {
   function handleDeletePost(id) {
     setPosts((posts) => posts.filter((post) => post.id !== id));
   }
+
+  useEffect(() => {
+    localStorage.setItem("posts", JSON.stringify(posts));
+  }, [posts])
+
+
   return (
     <div className="main">
       <MainNav />
